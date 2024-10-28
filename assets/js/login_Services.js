c22d0e5706fc9119c8f07097f9d62989169862b8
kaykyz2 <kaykytw@gmail.com>
2024-10-28 15:57:04 -0300

    Criação da page login, criação dos forms de login, com usuário e senha predefinidos com validações de campos, cadastro com validações de campos

---
+++ assets/js/login_Services.js
@@ -0,0 +1,38 @@
+function login() {
+    const usuario = document.getElementById("login-email").value
+    const senha = document.getElementById("login-senha").value
+
+    
+    if (usuario === "proz@gmail.com" && senha === "1234") {
+      alert("Login com sucesso!!!");
+      
+      window.location.href = "../home.html"
+    } else {
+      alert("Usuário ou senha incorretos.")
+    }
+
+    return false; 
+  }
+
+  function register() {
+    const nome = document.getElementById("register-name").value
+    const email = document.getElementById("register-email").value
+    const senha = document.getElementById("register-senha").value
+    const confirmaSenha = document.getElementById("register-confirma-senha").value 
+
+    if (senha == confirmaSenha && nome && email) {
+      alert(`Cadastro com sucesso :), Seja bem vindo: ${email}`);
+      window.location.href = "../home.html"
+    }
+    else if(senha != confirmaSenha) {
+
+        alert("As senhas não coicidem")
+
+    }
+    
+    else {
+      alert("huuum, preencha todos os campos!.");
+    }
+
+    return false;
+  }
