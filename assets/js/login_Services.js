02be695cac691275c299bca55438e037aa871939
Rafael Bezerra <rafael.bezerra92@gmail.com>
2024-11-28 16:58:42 -0300

    ajustes referentes a entrega da segunda sprint

--- assets/js/login_Services.js
+++ assets/js/login_Services.js
@@ -1,38 +1,34 @@
 function login() {
-    const usuario = document.getElementById("login-email").value
-    const senha = document.getElementById("login-senha").value
+  const usuario = document.getElementById("login-email").value
+  const senha = document.getElementById("login-senha").value
 
-    
-    if (usuario === "proz@gmail.com" && senha === "1234") {
-      alert("Login com sucesso!!!");
-      
-      window.location.href = "../home.html"
-    } else {
-      alert("Usuário ou senha incorretos.")
-    }
+  if (usuario === "proz@gmail.com" && senha === "1234") {
+    alert("Login com sucesso!!!");
 
-    return false; 
+    window.location.href = "../home.html"
+  } else {
+    alert("Usuário ou senha incorretos.")
   }
 
-  function register() {
-    const nome = document.getElementById("register-name").value
-    const email = document.getElementById("register-email").value
-    const senha = document.getElementById("register-senha").value
-    const confirmaSenha = document.getElementById("register-confirma-senha").value 
+  return false;
+}
 
-    if (senha == confirmaSenha && nome && email) {
-      alert(`Cadastro com sucesso :), Seja bem vindo: ${email}`);
-      window.location.href = "../home.html"
-    }
-    else if(senha != confirmaSenha) {
+function register() {
+  const nome = document.getElementById("register-name").value
+  const email = document.getElementById("register-email").value
+  const senha = document.getElementById("register-senha").value
+  const confirmaSenha = document.getElementById("register-confirma-senha").value
 
-        alert("As senhas não coicidem")
+  if (senha == confirmaSenha && nome && email) {
+    alert(`Cadastro com sucesso :), Seja bem vindo: ${email}`);
+    window.location.href = "../home.html"
+  } else if (senha != confirmaSenha) {
 
-    }
-    
-    else {
-      alert("huuum, preencha todos os campos!.");
-    }
+    alert("As senhas não coicidem")
 
-    return false;
+  } else {
+    alert("huuum, preencha todos os campos!.");
   }
+
+  return false;
+}
